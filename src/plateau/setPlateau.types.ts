8e974bfad1d33a806b0f712603b4434349b306b6
nandinip92 <nandini.p8892@gmail.com>
2023-07-31 22:56:03 +0100

    rename: Renaming the key variable name of the Boundary type

--- src/plateau/setPlateau.types.ts
+++ src/plateau/setPlateau.types.ts
@@ -3,6 +3,6 @@ export type Grid = [number, number];
 export type CoOrds = { x: number; y: number };
 
 export interface Boundary {
-  lowerBound: CoOrds;
-  upperBound: CoOrds;
+  lowerLeftCorner: CoOrds;
+  upperRightCorner: CoOrds;
 }
