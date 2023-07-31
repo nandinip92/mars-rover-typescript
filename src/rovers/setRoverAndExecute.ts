30ce3655d5b23c47cd47aab729080134d9035ca3
nandinip92 <nandini.p8892@gmail.com>
2023-07-31 20:21:14 +0100

    feat: Adding code to move the rover one step ahead in the direction it is facing

--- src/rovers/setRoverAndExecute.ts
+++ src/rovers/setRoverAndExecute.ts
@@ -1,7 +1,13 @@
-import { Boundary, CoOrds, Grid } from "../plateau/setPlateau.types";
+import { Boundary, Grid } from "../plateau/setPlateau.types";
 import { isInsidePlateau } from "../plateau/isInsidePlateau";
 import { Interface } from "readline";
 type compassDirections = "N" | "E" | "S" | "W";
+type compassDitectionType = {
+  N: Grid;
+  E: Grid;
+  W: Grid;
+  S: Grid;
+};
 
 interface RoverPosition {
   coOrds: Grid;
@@ -16,7 +22,12 @@ const turningDirection = {
 // To get to the next point in respective directions from (0,0).
 // i.e, To get to  respective direction from (X,Y)
 // North = (X,Y+1), East = (X+1,Y), West =(X-1,Y), South= (X,Y-1)
-const NEWS = { N: [0, 1], E: [1, 0], W: [-1, 0], S: [0, -1] };
+const NEWS: compassDitectionType = {
+  N: [0, 1],
+  E: [1, 0],
+  W: [-1, 0],
+  S: [0, -1],
+};
 
 /*
 *------------------------------*
@@ -33,17 +44,17 @@ export function setRoverAndExecute(
   roverInstructions: string
 ) {
   // Assigning the initial roverPosition to respective variables
-  let roverDirection = initialPosition.currentDirection;
-  let currentCoOrds = initialPosition.coOrds;
+  let roverDirection = initialPosition.currentDirection; //Eg: N
+  let currentCoOrds = initialPosition.coOrds; //Eg: [1,2]
   //Splitting the string of instructions into an array
-  let instructions = roverInstructions.split("");
+  let instructions = roverInstructions.split(""); //Eg: [L,M,L,M,L,M,L,M,M]
 
   //Looping each instruction and executing them [L,M,L,M,L,M,L,M,M]
   // if direction === M then move the rover in the current direction by 1
   // else only turn the rover into the direction
   instructions.forEach((direction) => {
     if (direction === "M") {
-      currentCoOrds = moveRover(currentCoOrds, roverDirection);
+      currentCoOrds = moveRover(plateauBoundary, currentCoOrds, roverDirection);
     } else {
       //roverDirection = turningDirection[direction][roverDirection];
       roverDirection =
@@ -54,16 +65,28 @@ export function setRoverAndExecute(
   });
 }
 
+/*
+moveRover function will move the rover one step ahead in the direction it is facing
+*/
 function moveRover(
+  plateauBoundary: Boundary,
   currentRoverCoOrds: Grid,
   currentDirection: compassDirections
 ): Grid {
   //get the coOrdinates of the respective direction
-  //add them to the currentRoverCoOrdinates
+  const roverDirection = NEWS[currentDirection]; //W:[0,1]
+
+  //add them to the currentRoverCoOrdinates [1,2]+[0,1] = [1+0,2+1]=[1,3]
+  const newX = currentRoverCoOrds[0] + roverDirection[0];
+  const newY = currentRoverCoOrds[1] + roverDirection[1];
+  const newPosition: Grid = [newX, newY];
+
   //check if the new Rover CoOrdinates are with in the plateau
   //If with in the plateau return new coOrdinates
   //else return old coordinates
-  return [1, 2];
+  return isInsidePlateau(plateauBoundary, newPosition)
+    ? newPosition
+    : currentRoverCoOrds;
 }
 function getNewDirection(
   turnDirection: "L" | "R",
