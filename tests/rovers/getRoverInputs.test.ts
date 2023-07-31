3c20e0d4c91562f85f8d19225b1707e548bd3b60
nandinip92 <nandini.p8892@gmail.com>
2023-07-31 11:18:29 +0100

    tests: updates the test cases with the new function signature and added new test case

--- tests/rovers/getRoverInputs.test.ts
+++ tests/rovers/getRoverInputs.test.ts
@@ -5,24 +5,28 @@ describe("getRoverInputs", () => {
     //Arrange, Act and Assert
     const boundary = { lowerBound: { x: 0, y: 0 }, upperBound: { x: 5, y: 5 } };
     expect(() => {
-      getRoverInputs(boundary, "12345");
+      getRoverInputs(boundary, "12345", "LMLMLMLM");
     }).toThrow(
       "Invalid input. Input must contain X and Y co-ordinates and a compass direction (N|E|S|W) seperated by space - X Y Direction"
     );
 
     expect(() => {
-      getRoverInputs(boundary, "1 3 5 2 7");
+      getRoverInputs(boundary, "1 3 5 2 7", "RMRMRMRM");
     }).toThrow(
       "Invalid input. Input must contain X and Y co-ordinates and a compass direction (N|E|S|W) seperated by space - X Y Direction"
     );
     expect(() => {
-      getRoverInputs(boundary, " 5 T N");
+      getRoverInputs(boundary, " 5 T N", "MMMMMMM");
     }).toThrow(
       "Invalid input. Input must contain X and Y co-ordinates and a compass direction (N|E|S|W) seperated by space - X Y Direction"
     );
 
     expect(() => {
-      getRoverInputs(boundary, " 3 4 Y");
+      getRoverInputs(boundary, " 3 4 Y", "RMLMRMLM");
     }).toThrow("Invalid input. compass direction must be (N|E|S|W)");
+
+    expect(() => {
+      getRoverInputs(boundary, " 3 4 Y", "RMLMKJRLM");
+    }).toThrow("Invalid Input, please enter Valid instructions [L | R |M]");
   });
 });
