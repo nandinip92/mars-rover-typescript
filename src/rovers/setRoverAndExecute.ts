75da4d40ab9c3c4efa9eaa2b50d6cab71997fe11
nandinip92 <nandini.p8892@gmail.com>
2023-07-31 19:07:11 +0100

    feat: Adding code to set the Rover

--- src/rovers/setRoverAndExecute.ts
+++ src/rovers/setRoverAndExecute.ts
@@ -1,17 +1,64 @@
-import { Boundary, CoOrds } from "../plateau/setPlateau.types";
+import { Boundary, CoOrds, Grid } from "../plateau/setPlateau.types";
+import { isInsidePlateau } from "../plateau/isInsidePlateau";
+import { Interface } from "readline";
 type compassDirections = "N" | "E" | "S" | "W";
-type RoverPosition = { coOrds: CoOrds; currentDirection: compassDirections };
+
+interface RoverPosition {
+  coOrds: Grid;
+  currentDirection: compassDirections;
+}
+
+const turningDirection = {
+  L: { N: "W", W: "S", S: "E", E: "N" },
+  R: { N: "E", E: "S", S: "W", W: "N" },
+};
+const NEWS = { N: [0, 1], E: [1, 0], W: [-1, 0], S: [0, -1] };
 
 /*
-*----------*
-|ALGORITHM |
-*----------*
-
+*------------------------------*
+|What setRoverAndExecute does? |
+*------------------------------*
+This function will get plateauBoundary, initialPosition and roverInstructions as inputs
+plateauBoundary is to check if the moving position is inside the plateau boundaries
 */
 
 //This function will check if the given input position of the rover is  valid or not
 export function setRoverAndExecute(
   plateauBoundary: Boundary,
-  initialPosition: Array<string | number>,
-  roverInstructions: Array<string>
-) {}
+  initialPosition: RoverPosition,
+  roverInstructions: string
+) {
+  // Assigning the initial roverPosition to respective variables
+  let roverDirection = initialPosition.currentDirection;
+  let currentCoOrds = initialPosition.coOrds;
+  //Splitting the string of instructions into an array
+  let instructions = roverInstructions.split("");
+
+  //Looping each instruction and executing them
+  // if direction === M then move the rover in the current direction by 1
+  // else only turn the rover into the direction
+  instructions.forEach((direction) => {
+    if (direction === "M") {
+      currentCoOrds = moveRover(currentCoOrds, roverDirection);
+    } else {
+      //roverDirection = turningDirection[direction][roverDirection];
+      roverDirection =
+        direction === "L"
+          ? getNewDirection("L", roverDirection)
+          : getNewDirection("R", roverDirection);
+    }
+  });
+}
+
+function moveRover(
+  currentCoOrds: Grid,
+  currentDirection: compassDirections
+): Grid {
+  return [1, 2];
+}
+function getNewDirection(
+  turnDirection: "L" | "R",
+  roverDirection: compassDirections
+): compassDirections {
+  return "N";
+}
