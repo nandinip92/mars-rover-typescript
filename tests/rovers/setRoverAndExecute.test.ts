df0cb978f9c97441f8736aaa11ee68c3f2b9613b
nandinip92 <nandini.p8892@gmail.com>
2023-07-31 21:27:47 +0100

    tests: Adding new test cases

--- tests/rovers/setRoverAndExecute.test.ts
+++ tests/rovers/setRoverAndExecute.test.ts
@@ -1,9 +1,9 @@
 import { setRoverAndExecute } from "../../src/rovers/setRoverAndExecute";
 
 describe("setRoverInitialPosition", () => {
-  test("Checking if rover is giving", () => {
+  test("Return the final position of the rover", () => {
     //Arrange
-    let boundary = { lowerBound: { x: 0, y: 0 }, upperBound: { x: 5, y: 5 } };
+    const boundary = { lowerBound: { x: 0, y: 0 }, upperBound: { x: 5, y: 5 } };
 
     //Act and Assert
     expect(
@@ -21,7 +21,10 @@ describe("setRoverInitialPosition", () => {
         "MMRMMRMRRM"
       )
     ).toBe("5 1 E");
+  });
 
+  test("Rover should not move outside the Plateau boundary", () => {
+    const boundary = { lowerBound: { x: 0, y: 0 }, upperBound: { x: 5, y: 5 } };
     expect(
       setRoverAndExecute(
         boundary,
@@ -29,5 +32,42 @@ describe("setRoverInitialPosition", () => {
         "MMMMMMMMM"
       )
     ).toBe("2 0 S");
+
+    expect(
+      setRoverAndExecute(
+        boundary,
+        { coOrds: [3, 3], currentDirection: "E" },
+        "MMMMMMMMM"
+      )
+    ).toBe("5 3 E");
+  });
+
+  test("Testing if the lower right corner of the plateau is not on origin (0,0)", () => {
+    const boundary = {
+      lowerBound: { x: -2, y: -2 },
+      upperBound: { x: 5, y: 5 },
+    };
+    expect(
+      setRoverAndExecute(
+        boundary,
+        { coOrds: [2, 3], currentDirection: "W" },
+        "MMMMMMMMM"
+      )
+    ).toBe("-2 3 W");
+    expect(
+      setRoverAndExecute(
+        boundary,
+        { coOrds: [-1, 3], currentDirection: "N" },
+        "MMMMMMMMM"
+      )
+    ).toBe("-1 5 N");
+
+    expect(
+      setRoverAndExecute(
+        boundary,
+        { coOrds: [-1, -1], currentDirection: "E" },
+        "MMLMRMM"
+      )
+    ).toBe("3 0 E");
   });
 });
