e1e169d1e002445374b32d3eefe7617ff13b015c
nandinip92 <nandini.p8892@gmail.com>
2023-07-31 21:13:59 +0100

    tests: Adding new test cases

--- tests/rovers/setRoverAndExecute.test.ts
+++ tests/rovers/setRoverAndExecute.test.ts
@@ -1,9 +1,9 @@
 import { setRoverAndExecute } from "../../src/rovers/setRoverAndExecute";
 
 describe("setRoverInitialPosition", () => {
-  test("Checking if the given position is valid string or not", () => {
+  test("Checking if rover is giving", () => {
     //Arrange
-    const boundary = { lowerBound: { x: 0, y: 0 }, upperBound: { x: 5, y: 5 } };
+    let boundary = { lowerBound: { x: 0, y: 0 }, upperBound: { x: 5, y: 5 } };
 
     //Act and Assert
     expect(
@@ -20,6 +20,14 @@ describe("setRoverInitialPosition", () => {
         { coOrds: [3, 3], currentDirection: "E" },
         "MMRMMRMRRM"
       )
-    ).toBe("5 2 E");
+    ).toBe("5 1 E");
+
+    expect(
+      setRoverAndExecute(
+        boundary,
+        { coOrds: [2, 3], currentDirection: "S" },
+        "MMMMMMMMM"
+      )
+    ).toBe("2 0 S");
   });
 });
