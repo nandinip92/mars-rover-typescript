a0712be4ef3d9e6840b540bf9a739e7f0e0b81a5
nandinip92 <nandini.p8892@gmail.com>
2023-07-31 11:27:16 +0100

    feat: changing function signature to accep Rover movement instructions

--- src/rovers/setRoverAndExecute.ts
+++ src/rovers/setRoverAndExecute.ts
@@ -12,5 +12,6 @@ type RoverPosition = { coOrds: CoOrds; currentDirection: compassDirections };
 //This function will check if the given input position of the rover is  valid or not
 export function setRoverAndExecute(
   plateauBoundary: Boundary,
-  initialPosition: Array<string | number>
+  initialPosition: Array<string | number>,
+  roverInstructions: Array<string>
 ) {}
