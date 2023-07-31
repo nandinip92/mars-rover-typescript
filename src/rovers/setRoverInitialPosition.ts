df751dfa2a237269e61e81257eac804d48b1b021
nandinip92 <nandini.p8892@gmail.com>
2023-07-31 07:04:13 +0100

    fix:Changed the comments

--- src/rovers/setRoverInitialPosition.ts
+++ src/rovers/setRoverInitialPosition.ts
@@ -6,9 +6,7 @@ type RoverPosition = { coOrds: CoOrds; currentDirection: compassDirections };
 *----------*
 |ALGORITHM |
 *----------*
-1. Rover position must be x and Y cooradinates along with Direction it is facing ie., 3 4 E
-    (1a) As the given input is String split the input and chek if the first two elements are 'numbers' 
-        and the last element must be any of the compass directions.
+
 */
 
 //This function will check if the given input position of the rover is  valid or not
