108e3b57d135ac3289cb40998e41f133f38dcef0
nandinip92 <nandini.p8892@gmail.com>
2023-07-31 19:08:05 +0100

    tests: modifying the test cases according to the new function signature

--- tests/rovers/setRoverAndExecute.test.ts
+++ tests/rovers/setRoverAndExecute.test.ts
@@ -5,11 +5,21 @@ describe("setRoverInitialPosition", () => {
     //Arrange
     const boundary = { lowerBound: { x: 0, y: 0 }, upperBound: { x: 5, y: 5 } };
 
-    const output1 = { coOrds: [1, 2], currentDirection: "N" };
     //Act and Assert
-    expect(setRoverAndExecute(boundary, [1, 2, "N"])).toBe(output1);
+    expect(
+      setRoverAndExecute(
+        boundary,
+        { coOrds: [1, 2], currentDirection: "N" },
+        "LMLMLMLMM"
+      )
+    ).toBe("1 3 N");
 
-    const output2 = { coOrds: [3, 3], currentDirection: "E" };
-    expect(setRoverAndExecute(boundary, [3, 3, "E"])).toBe(output2);
+    expect(
+      setRoverAndExecute(
+        boundary,
+        { coOrds: [3, 3], currentDirection: "E" },
+        "MMRMMRMRRM"
+      )
+    ).toBe("5 2 E");
   });
 });
