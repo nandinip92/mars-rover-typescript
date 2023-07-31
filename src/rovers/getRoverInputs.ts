cbcd841ea8fa3f7b10dbcb32bc631a7b1b3a50e9
nandinip92 <nandini.p8892@gmail.com>
2023-07-31 11:17:19 +0100

    feat: adding code to validate instructions and changed the function signature to accept instructions as well

--- src/rovers/getRoverInputs.ts
+++ src/rovers/getRoverInputs.ts
@@ -1,49 +1,68 @@
 import { Boundary, CoOrds } from "../plateau/setPlateau.types";
+import { isInsidePlateau } from "../plateau/isInsidePlateau";
 import { setRoverAndExecute } from "../rovers/setRoverAndExecute";
+
+// type compassDirections = "N" | "E" | "S" | "W";
+// type RoverPosition = { coOrds: CoOrds; currentDirection: compassDirections };
 
 /*
 *----------*
-|ALGORITHM |
+|PsudoCode |
 *----------*
-1. Rover position must be x and Y cooradinates along with Direction it is facing ie., 3 4 E
-    (1a) As the given input is String split the input and chek if the first two elements are 'numbers' 
-        and the last element must be any of the compass directions.
+1. 'getRoverInputs' fucntion takes plateauBoundary, initialPosition and instructions as inputs.
+  (1a) Rover position must be x and Y cooradinates along with Direction it is facing ie., 3 4 E
+    (i) As the given input('initialPosition') is String split the input and check if the first two elements are 'numbers' 
+        and the last element must be any of the compass directions.->checkGivenPositionValidity(givenPosition).
+  (1b). Rover instructions must contain only Left,Right and Move (L,R,M) ->isValidInstruction()
+
+  (1c). If both the Inputs to the rover are valid then we will place the rover in the given position and start executing instructions.
+        setRoverAndExecute()
 */
 
 //This function will check if the given input position of the rover is  valid or not
 export function getRoverInputs(
   plateauBoundary: Boundary,
-  initialPosition: string
+  initialPosition: string,
+  instructions: string
 ) {
-  let currentPosition = initialPosition
-    .replace(/\s+/g, " ")
+  let givenPosition = initialPosition
+    .replace(/\s+/g, " ") //If the given input has more spaces between the characters this will replace them into one space
     .trim()
     .split(" ")
-    .map((ele) => (isNaN(parseInt(ele)) ? ele : parseInt(ele))); //Convering to numbers
+    .map((ele) => (isNaN(parseInt(ele)) ? ele : parseInt(ele))); //Convering digits from string to number and leaves strings as is
 
-  if (checkCurrentPositionValidity(currentPosition)) {
-    setRoverAndExecute(plateauBoundary, currentPosition);
+  const roverInstructions = instructions.split("");
+
+  if (isValidPositon(givenPosition) && isValidInstruction(roverInstructions)) {
+    setRoverAndExecute(plateauBoundary, givenPosition, instructions);
   }
 }
 
-function checkCurrentPositionValidity(
-  currentPosition: Array<string | number>
-): boolean {
-  if (currentPosition.length < 3 || currentPosition.length > 3)
+function isValidPositon(givenPosition: Array<string | number>): boolean {
+  if (givenPosition.length < 3 || givenPosition.length > 3)
     throw new Error(
       "Invalid input. Input must contain X and Y co-ordinates and a compass direction (N|E|S|W) seperated by space - X Y Direction"
     );
   if (
-    typeof currentPosition[2] === "number" ||
-    typeof currentPosition[1] === "string" ||
-    typeof currentPosition[1] === "string"
+    typeof givenPosition[2] === "number" ||
+    typeof givenPosition[1] === "string" ||
+    typeof givenPosition[1] === "string"
   )
     throw new Error(
       "Invalid input. Input must contain X and Y co-ordinates and a compass direction (N|E|S|W) seperated by space - X Y Direction"
     );
   let directions = ["N", "E", "S", "W"];
-  if (!directions.includes(currentPosition[2]))
+  if (!directions.includes(givenPosition[2]))
     throw new Error("Invalid input. compass direction must be (N|E|S|W)");
 
+  return true;
+}
+
+function isValidInstruction(roverInstructions: Array<string>): boolean {
+  const isValid = roverInstructions.map((i) => ["L", "R", "M"].includes(i));
+  if (!isValid.every(Boolean))
+    throw new Error(
+      "Invalid Input, please enter Valid instructions [L | R |M]"
+    );
   return true;
 }
