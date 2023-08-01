f0a925f272dde17338c755a0160d6005f0547a05
nandinip92 <nandini.p8892@gmail.com>
2023-08-01 22:50:26 +0100

    refactor: refactoring of the code with return statement

--- src/plateau/getPlateauCorners.ts
+++ src/plateau/getPlateauCorners.ts
@@ -24,18 +24,19 @@ export function getPlateauInputs(inputCoOrds: string): void {
     return startMission();
   }
 
-  if (coOrdinates.length === 2) {
-    const xCoOrds: number = coOrdinates[0] as number;
-    const yCoOrds: number = coOrdinates[1] as number;
-    isValidCoOrdinates([xCoOrds, yCoOrds]); //passing only upperRight coordinates
-  }
-  if (coOrdinates.length === 4) {
-    const x1: number = coOrdinates[0] as number;
-    const y1: number = coOrdinates[1] as number;
-    const x2: number = coOrdinates[2] as number;
-    const y2: number = coOrdinates[3] as number;
-    isValidCoOrdinates([x2, y2], [x1, y1]); //passing upperRight coordinates and lower-left coordinates
-  }
+  // if coOrdinates.length === 2 then pass only upperRight coordinates into 'isValidCoOrdinates()'
+  // else pass both upperRight coordinates and lowerLeft coordinates into 'isValidCoOrdinates()'
+  // return value will assigned to 'plateauCorners' variable
+  const plateauCorners: PlateauCorners | void =
+    coOrdinates.length === 2
+      ? isValidCoOrdinates([coOrdinates[0] as number, coOrdinates[1] as number])
+      : isValidCoOrdinates(
+          [coOrdinates[2] as number, coOrdinates[3] as number],
+          [coOrdinates[0] as number, coOrdinates[1] as number]
+        );
+  if (plateauCorners === undefined) return; //if the coOrdinates are invalid 'isValidCoOrdinates()' will return undefined
+
+  return;
 }
 
 /*
@@ -56,8 +57,10 @@ Just in case if in future lowerleft co0ordinates are not fixed on origin[0,0], s
 lowerLeftCorner --> OptionalInput,
 upperRightCorner --->Input
 */
-function isValidCoOrdinates(upperRightCorner: Grid, lowerLeftCorner?: Grid) {
-  console.log(isValidCoOrdinates.name);
+function isValidCoOrdinates(
+  upperRightCorner: Grid,
+  lowerLeftCorner?: Grid
+): PlateauCorners | void {
   const lowerLeft = lowerLeftCorner === undefined ? [0, 0] : lowerLeftCorner;
   //upper right coordinates must always be greater than the lower left coordinates
   if (
@@ -75,5 +78,5 @@ function isValidCoOrdinates(upperRightCorner: Grid, lowerLeftCorner?: Grid) {
     lowerLeftCorner: { x: lowerLeft[0], y: lowerLeft[1] },
     upperRightCorner: { x: upperRightCorner[0], y: upperRightCorner[1] },
   };
-  console.log(plateauCorners);
+  return plateauCorners;
 }
