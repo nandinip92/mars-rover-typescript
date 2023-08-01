206a080623cc856e2707381408a3c0458f7f972b
nandinip92 <nandini.p8892@gmail.com>
2023-08-01 20:07:36 +0100

    create: New file to get the plateau data and validate

---
+++ src/plateau/getPlateauCorners.ts
@@ -0,0 +1,7 @@
+import { clear, print, askQuestion } from "../ui/console";
+import { Grid, plateauShape, PlateauCorners } from "./plateau.types";
+import { RoverPosition } from "../rovers/rover.types";
+
+export function getPlateauCorners(inputCoOrds: string) {
+  console.log(inputCoOrds, typeof inputCoOrds);
+}
