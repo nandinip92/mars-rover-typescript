45baa7a6e9f806b4fe33ae231fa7618d5cd861c4
nandinip92 <nandini.p8892@gmail.com>
2023-08-01 20:06:34 +0100

    feat: Adding print statements

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { clear, print, askQuestion } from "./ui/console";
-import { Grid, plateauShape, PlateauCorners } from "./plateau/plateau.types";
-import { RoverPosition } from "./rovers/rover.types";
+import { getPlateauCorners } from "./plateau/getPlateauCorners";
 
 function welcomeToMarsMission(): void {
   clear(false);
@@ -12,12 +11,24 @@ function welcomeToMarsMission(): void {
 }
 
 function startMission() {
-  const plateauBoundraies = getPlateauCoOrdinates();
-  const roverPositiosn = getRoverPosition();
-  const instructionsToRover = getInstructions();
+  print(
+    `❗NOTE: We need lower-left co-ordinates and upper-right coordinates of the Plateau.
+    \tIf only one coordinate is given then, it will be considered as upper-right corner,
+    \tlower-left corner of the plateau will be consider as origin i.e (0,0).
+    \tGive the coOrdinates in the order as lower-left coordinates and then upper-right coordinates.
+    `
+  );
+
+  askQuestion(
+    "Enter the plateau co-ordinates seperated with spaces Eg: X Y",
+    getPlateauCorners
+  );
+  // const plateauCorners = getPlateauCoOrdinates();
+  // const roverPosition = getRoverPosition();
+  // const instructionsToRover = getInstructions();
   //setRoverAndExecute(plateauBoundraies, roverPositiosn, instructionsToRover);
 }
-function getPlateauCoOrdinates() {}
+
 function getRoverPosition() {}
 function getInstructions() {}
 
