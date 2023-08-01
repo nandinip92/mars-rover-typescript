7dd0aab6a9f587bc1be4bb4b89176ba9f0790ad1
nandinip92 <nandini.p8892@gmail.com>
2023-08-01 08:55:40 +0100

    rename and feat:Renameing the function from 'isInsidePlateau' to 'isOnPlateau'. Adding code to handle Square and Rectangle shape seperately and adding new plateau Shape argument into the function 'isOnPlateau'

--- src/plateau/isInsidePlateau.ts
+++ src/plateau/isInsidePlateau.ts
@@ -1,20 +1,37 @@
-import { Grid, Boundary } from "./setPlateau.types";
+import { Grid, Boundary, plateauShape } from "./setPlateau.types";
 /*
 This function is to check if the given coordinates is inside the plateau or not.
+For Square or Rectangle Plateau
 It is valid if they are inside the plateau boundary
 lowerBound.x <= coOrdinate.x <= upperBound.x && lowerBound.y<=coOrdinate.y<= upperBound.y
 */
 
-export function isInsidePlateau(
+export function isOnPlateau(
   plateauBoundary: Boundary,
-  coOrdinate: Grid
+  plateauShape: plateauShape,
+  newCoOrdinate: Grid
+): Boolean {
+  let isValidCoOrdinate: Boolean = false;
+  //Checking if the shape of the plateau is Square or Rectanle
+  if (plateauShape === "Square" || plateauShape === "Rectangle") {
+    isValidCoOrdinate = isOnSquareOrRectanglePlateau(
+      plateauBoundary,
+      newCoOrdinate
+    );
+  }
+  return isValidCoOrdinate;
+}
+
+function isOnSquareOrRectanglePlateau(
+  plateauBoundary: Boundary,
+  newCoOrdinate: Grid
 ): Boolean {
   const x =
-    plateauBoundary.lowerLeftCorner.x <= coOrdinate[0] &&
-    coOrdinate[0] <= plateauBoundary.upperRightCorner.x;
+    plateauBoundary.lowerLeftCorner.x <= newCoOrdinate[0] &&
+    newCoOrdinate[0] <= plateauBoundary.upperRightCorner.x;
   const y =
-    plateauBoundary.lowerLeftCorner.y <= coOrdinate[1] &&
-    coOrdinate[1] <= plateauBoundary.upperRightCorner.y;
+    plateauBoundary.lowerLeftCorner.y <= newCoOrdinate[1] &&
+    newCoOrdinate[1] <= plateauBoundary.upperRightCorner.y;
 
   return x && y;
 }
