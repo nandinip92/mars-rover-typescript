c9edd955b856dd1bd8bc7a214a3f722861842335
nandinip92 <nandini.p8892@gmail.com>
2023-08-02 18:17:47 +0100

    refactor

--- src/plateau/plateau.types.ts
+++ src/plateau/plateau.types.ts
@@ -4,10 +4,10 @@ export type Grid = [number, number];
 
 export type CoOrds = { x: number; y: number };
 
-export interface PlateauCorners {
+export type PlateauCorners = {
   lowerLeftCorner: CoOrds;
   upperRightCorner: CoOrds;
-}
+};
 
 type obstacles = Array<Grid>;
 export let obstaclesOnPlateau: obstacles = [];
