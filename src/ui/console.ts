e2d39ca6735ee34d9d786cd47c5e35b5f053e11f
nandinip92 <nandini.p8892@gmail.com>
2023-08-02 17:17:32 +0100

    feat: adding 'readline-syc' to get the user inputs synchronously

--- src/ui/console.ts
+++ src/ui/console.ts
@@ -16,13 +16,9 @@ export function clear(addTopBorder: boolean): void {
   }
 }
 
-// NOTE: this "createInterface" function is built into node and is referring to the console interface - NOT a TypeScript interface!
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const readlineSync = require(`readline-sync`);
 
 // this function allows us to prompt the user with a question, and call a callback function with whatever string has been input
-export function askQuestion(question: string, callback: (arg: string) => void) {
-  reader.question(`❓ ${question} 👉 `, callback);
+export function askQuestion(question: string) {
+  return readlineSync.question(`❓ ${question} 👉 `);
 }
