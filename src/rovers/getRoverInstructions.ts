10f94cfef88ab15a65a276853bc5ccd9906335c4
nandinip92 <nandini.p8892@gmail.com>
2023-08-03 00:01:16 +0100

    feat: Adding logic to validate rover instructions

--- src/rovers/getRoverInstructions.ts
+++ src/rovers/getRoverInstructions.ts
@@ -1,3 +1,29 @@
-export function getRoverInstructions(instruction: string): string {
-  return "hello";
+import { askQuestion, print } from "../ui/console";
+
+/*
+`isValidInstruction` checks if the instruction is valid or not.
+
+Instruction must contain only L,R and M
+*/
+let validInstruction = "";
+export function getRoverInstructions() {
+  const instruction = askQuestion("Enter Rover instructions");
+  isValidInstruction(instruction);
+  return validInstruction;
+}
+
+function isValidInstruction(instructions: string): string {
+  const roverInstructions = instructions.replace(/\s+/g, "");
+  const isValidInstruction = roverInstructions
+    .split("")
+    .map((i) => ["L", "R", "M"].includes(i));
+
+  if (!isValidInstruction.every(Boolean)) {
+    print(
+      "🚫🚫🚫Invalid Input, please enter Valid instructions [L | R |M] eEg:LMLLRMM🚫🚫🚫"
+    );
+    return getRoverInstructions(); // ❌ERROR: so get correct inputs
+  }
+  validInstruction = roverInstructions;
+  return validInstruction;
 }
