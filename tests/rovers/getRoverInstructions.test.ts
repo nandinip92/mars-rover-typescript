b54ab724c3c7a5780fdac73d06b3203f9d864963
nandinip92 <nandini.p8892@gmail.com>
2023-08-05 22:00:19 +0100

    create: creating a new tests file for rover Instruction

---
+++ tests/rovers/getRoverInstructions.test.ts
@@ -0,0 +1,6 @@
+import { getRoverInstructions } from "../../src/rovers/getRoverInstructions";
+
+describe("getRoverInstructions", () => {
+  test("Return Valid Instruction String", () => {});
+  test("Return ERROR for inavlid Instruction String", () => {});
+});
