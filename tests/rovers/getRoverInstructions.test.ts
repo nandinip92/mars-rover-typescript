c2410d0d5946f63cb0b59d8b771d054fab278091
nandinip92 <nandini.p8892@gmail.com>
2023-08-05 22:07:54 +0100

    tests: Adding new tests for rover Instructions

--- tests/rovers/getRoverInstructions.test.ts
+++ tests/rovers/getRoverInstructions.test.ts
@@ -1,6 +1,15 @@
-import { getRoverInstructions } from "../../src/rovers/getRoverInstructions";
+import { isValidInstruction } from "../../src/rovers/getRoverInstructions";
 
 describe("getRoverInstructions", () => {
-  test("Return Valid Instruction String", () => {});
-  test("Return ERROR for inavlid Instruction String", () => {});
+  test("Return Valid Instruction String", () => {
+    expect(isValidInstruction("LMLMLMLMLM")).toBe("LMLMLMLMLM");
+    expect(isValidInstruction("LMLRMRMLMLM")).toBe("LMLRMRMLMLM");
+    expect(isValidInstruction("MMRMMRMRRM")).toBe("MMRMMRMRRM");
+  });
+  test("Return ERROR for inavlid Instruction String", () => {
+    expect(isValidInstruction("LKMN")).toBe("INVALID_ROVER_INSTRUCTION");
+    expect(isValidInstruction("jsdaksdhashfoi")).toBe(
+      "INVALID_ROVER_INSTRUCTION"
+    );
+  });
 });
