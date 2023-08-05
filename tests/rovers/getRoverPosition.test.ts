9814b1e1f2b545ce18f1dd2780cc78f20c5f82f0
nandinip92 <nandini.p8892@gmail.com>
2023-08-05 21:14:34 +0100

    tests:  Adding new tests

--- tests/rovers/getRoverPosition.test.ts
+++ tests/rovers/getRoverPosition.test.ts
@@ -2,6 +2,7 @@ import { getRoverPosition } from "../../src/rovers/getRoverInputs";
 
 describe("getPlateauInputs", () => {
   test("Returns Valid rover Position", () => {
+    //ARRANGE
     let plateauCorner = {
       lowerLeftCorner: { x: 0, y: 0 },
       upperRightCorner: { x: 5, y: 5 },
@@ -10,6 +11,91 @@ describe("getPlateauInputs", () => {
       coOrds: [1, 2],
       currentDirection: "N",
     };
-    expect(getRoverPosition(plateauCorner, "SQUARE", "1 2 N")).toEqual(output);
+    //ACT
+    let funcOutput = getRoverPosition(plateauCorner, "SQUARE", "1 2 N");
+    //ASSERT
+    expect(funcOutput).toEqual(output);
+
+    output = {
+      coOrds: [3, 3],
+      currentDirection: "E",
+    };
+    //ASSERT
+    funcOutput = getRoverPosition(plateauCorner, "SQUARE", "3    3  E");
+    expect(funcOutput).toEqual(output);
+
+    //ARRANGE
+    plateauCorner = {
+      lowerLeftCorner: { x: 4, y: 4 },
+      upperRightCorner: { x: 10, y: 10 },
+    };
+    output = {
+      coOrds: [6, 5],
+      currentDirection: "S",
+    };
+    //ACT
+    funcOutput = getRoverPosition(plateauCorner, "SQUARE", " 6 5 S ");
+    //ASSERT
+    expect(funcOutput).toEqual(output);
+    plateauCorner = {
+      lowerLeftCorner: { x: 0, y: 0 },
+      upperRightCorner: { x: 10, y: 4 },
+    };
+    output = {
+      coOrds: [7, 1],
+      currentDirection: "W",
+    };
+    funcOutput = getRoverPosition(plateauCorner, "RECTANGLE", " 7 1 W ");
+    expect(funcOutput).toEqual(output);
+  });
+
+  test("Returns Error if the invalid Input format is given", () => {
+    //ARRANGE
+    let plateauCorner = {
+      lowerLeftCorner: { x: 0, y: 0 },
+      upperRightCorner: { x: 5, y: 5 },
+    };
+    //ACT
+    let funcOutput = getRoverPosition(plateauCorner, "SQUARE", "hello!! ");
+    //ASSERT
+    expect(funcOutput).toEqual("INVALID_ROVER_POSITION");
+
+    //ACT
+    funcOutput = getRoverPosition(plateauCorner, "SQUARE", "1234 ");
+    //ASSERT
+    expect(funcOutput).toEqual("INVALID_ROVER_POSITION");
+
+    //ACT
+    funcOutput = getRoverPosition(plateauCorner, "SQUARE", "12 N N ");
+    //ASSERT
+    expect(funcOutput).toEqual("INVALID_ROVER_POSITION");
+
+    //ACT
+    funcOutput = getRoverPosition(plateauCorner, "SQUARE", "1 2 N N ");
+    //ASSERT
+    expect(funcOutput).toEqual("INVALID_ROVER_POSITION");
+  });
+  test("Returns Error if the given coOrdinates are not on the plateau", () => {
+    //ARRANGE
+    let plateauCorner = {
+      lowerLeftCorner: { x: 0, y: 0 },
+      upperRightCorner: { x: 5, y: 5 },
+    };
+    //ACT
+    let funcOutput = getRoverPosition(plateauCorner, "SQUARE", " 6 5 S ");
+
+    //ASSERT
+    expect(funcOutput).toBe("INVALID_ROVER_POSITION");
+
+    //ARRANGE
+    plateauCorner = {
+      lowerLeftCorner: { x: 0, y: 0 },
+      upperRightCorner: { x: 10, y: 4 },
+    };
+    //ACT
+    funcOutput = getRoverPosition(plateauCorner, "RECTANGLE", "12  5 N ");
+
+    //ASSERT
+    expect(funcOutput).toBe("INVALID_ROVER_POSITION");
   });
 });
